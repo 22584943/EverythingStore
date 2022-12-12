17692dec8cd06a148ff658178ebfc5152d697bf3
22584943 <117286267+22584943@users.noreply.github.com>
2022-12-12 17:07:21 +0000

    added login route

--- Main.java
+++ Main.java
@@ -8,8 +8,10 @@ class Main {
     public static void main(String[] args) throws IOException {
 
         HttpServer server = HttpServer.create(new InetSocketAddress(PORT),0);
+        server.createContext("/", new RootHandler() );
         server.createContext("/customers", new CustomerHandler() );
         server.createContext("/products", new ProductHandler() );
+        server.createContext("/login", new LoginHandler() );
         server.setExecutor(null);
         server.start();
         System.out.println("The server is listening on port " + PORT);
